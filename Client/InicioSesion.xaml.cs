45ef77357efcb3f95769422d1c276b12f975a8f2
NewKevinK <zS19014047@estudiantes.uv.mx>
2022-09-19 17:05:22 -0500

    19/09 Estructura y Ventanas

--- Client/InicioSesion.xaml.cs
+++ Client/InicioSesion.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,21 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UV_Eats_Client
+namespace UV_Eats_Client.Client
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para InicioSesion.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InicioSesion : Window
     {
-        public MainWindow()
+        public InicioSesion()
         {
             InitializeComponent();
-            //tEST
-            //test2
         }
     }
 }
---
+++ Logic/Secure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UV_Eats_Client.Logic
+{
+    class Secure
+    {
+    }
+}
