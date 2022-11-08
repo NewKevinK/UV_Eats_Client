2c94d17bade3b897d2a0e1c0ab981f11f030f798
NewKevinK <zS19014047@estudiantes.uv.mx>
2022-11-08 02:24:10 -0600

    Pantalla Inicial, Login - 8/11/22

---
+++ Client/PantallaInicial.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UV_Eats_Client.Client
+{
+    /// <summary>
+    /// Lógica de interacción para PantallaInicial.xaml
+    /// </summary>
+    public partial class PantallaInicial : Window
+    {
+        public PantallaInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
