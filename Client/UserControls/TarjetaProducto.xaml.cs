9c43392457f7671b8e2132f5ac9d5a994068d99c
Samuel <samy.srzcln@gmail.com>
2022-12-12 15:20:47 -0600

    comit prueba 1

--- Client/UserControls/TarjetaProducto.xaml.cs
+++ Client/UserControls/TarjetaProducto.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -11,7 +10,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using UV_Eats_Client.Logic;
 using UV_Eats_Client.Models;
 
 namespace UV_Eats_Client.Client.UserControls
@@ -21,7 +19,6 @@ namespace UV_Eats_Client.Client.UserControls
     /// </summary>
     public partial class TarjetaProducto : UserControl
     {
-        int idproducto;
         public TarjetaProducto()
         {
             InitializeComponent();
@@ -29,7 +26,6 @@ namespace UV_Eats_Client.Client.UserControls
         }
         public TarjetaProducto(Producto newproducto)
         {
-            idproducto=newproducto.idProducto;
             InitializeComponent();
             nombreProducto.Content = newproducto.nombre;
             descripcionProducto.Content = newproducto.descripcion;
@@ -38,23 +34,6 @@ namespace UV_Eats_Client.Client.UserControls
             numlikes.Content = newproducto.numLike;
             Uri fileUri = new Uri(newproducto.imagenProducto);
             imagenProductotar.Source = new BitmapImage(fileUri);
-
-        }
-
-        private void agregarCarrito(object sender, RoutedEventArgs e)
-        {
-            
-            var padre = Window.GetWindow(this) as UV_Eats_Client.Client.PantallaInicial;
-
-            if (padre != null)
-            {
-                padre.agregarALCARRO(idproducto);
-
-            }
-        }
-
-        private void ordenarYa(object sender, ContextMenuEventArgs e)
-        {
 
         }
     }
