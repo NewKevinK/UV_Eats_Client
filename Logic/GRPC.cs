96e9fcda401dacad92e926d19386ace1842633d5
Samuel <samy.srzcln@gmail.com>
2022-12-12 15:18:08 -0600

    czs

--- Logic/GRPC.cs
+++ Logic/GRPC.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+//using Grpc.Core.Client;
+using Grpc.Net.Client;
 
 namespace UV_Eats_Client.Logic
 {
     class GRPC
     {
+        static void main(string[] args)
+        {
+            /*var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
 
-        /*
-         var channel = GrpcChannel.ForAddress("https://localhost:5001");
-var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(
+            new HelloRequest { Name = "World" });
 
-var response = await client.SayHelloAsync(
-    new HelloRequest { Name = "World" });
+            Console.WriteLine(response.Message); */
+        }
 
-Console.WriteLine(response.Message);
-         */
+        
     }
 }
